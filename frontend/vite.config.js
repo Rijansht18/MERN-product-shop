9023c49852fd1f527653dee36544cff5cf967aee
Rijan Shrestha <rijan0518@gmail.com>
2025-01-03 17:29:02 +0545

    up

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    proxy:{
-      "/api":{
-        target:"http://localhost:5000"
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000"
       }
     }
   },
   build: {
-    outDir: 'public', // Change output directory to 'public'
+    outDir: 'dist', // Change output directory to 'dist' for Vercel
+    publicDir: 'public',
   },
 })
